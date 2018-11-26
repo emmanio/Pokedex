8bb0b21d3700a353ec0a2408b3774d7bfc5920be
emma-nio <emmanioashley@gmail.com>
2018-11-26 22:31:56 +1300

    Pokemon Data

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,10 +1,10 @@
 class Pokemon {
   constructor(data) {
-    this.id = data.id;
-    this.name = data.name;
-    this.sprite = data.sprites.front_default;
-    this.type = data.types[0].type.name;    
+    this.id = data.id
+    this.name = data.name
+    this.sprite = data.sprites.front_default
+    this.type = data.types[0].type.name
   }
 }
 
-export default Pokemon;
+export default Pokemon

92da19946bd0931427cabffa8b2f09a87c14b4cc
emma-nio <emmanioashley@gmail.com>
2018-11-26 22:13:20 +1300

    added event handler

--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -2,11 +2,10 @@ import React from 'react'
 import sprites from '../assets/sprites.png'
 import './styles/PokeCell.css'
 
-const PokeCell = ({ pokeClass }) => {
+const PokeCell = ({ pokeClass, handleClick }) => {
   const { id, backgroundPosition } = pokeClass
   const style = { backgroundImage: `url(${sprites})`, backgroundPosition }
-
-  return <button style={style} className="poke-cell"></button>
+  return <button onClick={() => handleClick(id)} style={style} className="poke-cell"></button>
 }
 
 export default PokeCell;

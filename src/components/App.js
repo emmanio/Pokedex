977e06522315e8c9dbde9b54e6d755706d68bd11
emma-nio <emmanioashley@gmail.com>
2018-11-26 22:31:43 +1300

    added api request to handleClick

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,32 @@ import './styles/App.css'
 
 import PokeList from './PokeList.js'
 import DetailView from './DetailView.js'
+import Pokemon from '../Pokemon.js'
 
 class App extends Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      pokemon: {}
+    }
     this.handleClick=this.handleClick.bind(this)
   }
 
   handleClick (id) {
-    console.log(id)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(res => res.json())
+    .then(data => {
+      const pokemon = new Pokemon(data)
+      this.setState({pokemon})
+    })   
+    .catch(err => console.log(err))
   }
+
   render () {
     return (
       <div className="App">
       <PokeList handleClick={this.handleClick}/>
-      <DetailView/>
+      <DetailView pokemon={this.state.pokemon}/>
       </div>
     )
   }

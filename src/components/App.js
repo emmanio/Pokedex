c8825a6cd2f8dc7a2ce155a01396d3cdbb804679
emma-nio <emmanioashley@gmail.com>
2018-11-26 21:42:59 +1300

    imported PokeList into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import './styles/App.css'
 
+import PokeList from './PokeList.js'
+
 class App extends Component {
   constructor (props) {
     super(props)
@@ -9,7 +11,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-
+      <PokeList/>
       </div>
     )
   }
